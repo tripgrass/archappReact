fbd89526f5387e34b9207907e4d7b3710107fc5d
tripgrass <wjgrass@gmail.com>
2026-02-11 08:09:10 -0700

    add webview to post page

--- components/MetaView.tsx
+++ components/MetaView.tsx
@@ -31,8 +31,7 @@ export default function MetaView({ artifactId, image, slideoutMetaState, setSlid
 		var source = post?.content ? post?.content : "<p>empty</p>";
 		var source = `<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		<div style='height:100vh; width:100%;'>
-		<h1 style="">` + metaObject.post_title + `</H1>
-		` + source + `</div>`;
+		<h1 style="">` + metaObject.post_title + `</H1>` + source + `</div>`;
 
 const tagsStyles = {
   body: {
